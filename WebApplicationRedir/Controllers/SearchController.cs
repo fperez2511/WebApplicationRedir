92c837f65ebce70bb8941ba0d7f2db6df9c3fa6e
Fabricio Perez <fperez@fusionhit.com>
2023-06-08 09:03:14 -0600

    Add project files.

---
+++ WebApplicationRedir/Controllers/SearchController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationRedir.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: SearchController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string criteria)
+        {
+            ViewBag.SearchResults = criteria;
+            return View();
+            //return RedirectToAction(nameof(Index));
+        }
+
+        // GET: SearchController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SearchController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SearchController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SearchController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SearchController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SearchController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SearchController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplicationRedir/wwwroot/js/site.js
@@ -0,0 +1,47 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    // Search
+    $('#btnSearch').unbind('click').bind('click', function (event) {
+        event.preventDefault();
+
+        let data = $('#txtCriteria').val();
+        if (data === '') {
+            alert('Please enter search criteria!');
+            return true;
+        }
+        //alert('DEBUG! ' + data);
+        console.log('[DEBUG] calling Search controller with payload: ' + data);
+
+        $.ajax({
+            type: "POST",
+            url: "/Search",
+            data: { criteria: data },
+            success: function (result) {
+                //$(document).load("/Search/Index", result);
+                //console.log(result);
+                document.open();
+                document.write(result);
+                document.close();
+            },
+            error: function (xhr) {
+                alert('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
+            }
+        });
+
+        //var jqxhr = $.post("/Search", { criteria: data }, function () {
+        //    alert("success");
+        //})
+        //    .done(function () {
+        //        alert("second success");
+        //    })
+        //    .fail(function () {
+        //        alert("error");
+        //    })
+        //    .always(function () {
+        //        alert("finished");
+        //    });
+    });
+});
